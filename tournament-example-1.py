29d2fda221bc3307445706886b62d700a26f473b
akalivm <2722968218@qq.com>
2022-01-23 18:19:37 +0000

    add Shubik, SteinAndRapoport, Grudger

--- tournament-example-1.py
+++ tournament-example-1.py
@@ -3,11 +3,11 @@ from tournament.agents.random import Random
 from tournament.agents.tft import TitForTat, OmegaTFT, TFTT, TTFT, GradualTFT, GenerousTFT
 from tournament.agents.pavlov import Pavlov
 from tournament.tournament import RoundRobinTournament
-from tournament.agents.axelrod_first import Davis, Graaskamp
+from tournament.agents.axelrod_first import Davis, Graaskamp, Shubik, SteinAndRapoport, Grudger
 
 
 if __name__ == "__main__":
-    tournament = RoundRobinTournament([TitForTat, Random, AllC, AllD, Davis, Graaskamp, OmegaTFT, Pavlov, TFTT, TTFT, GradualTFT, GenerousTFT])
+    tournament = RoundRobinTournament([TitForTat, Random, AllC, AllD, Davis, Graaskamp, Shubik, SteinAndRapoport, Grudger, OmegaTFT, Pavlov, TFTT, TTFT, GradualTFT, GenerousTFT])
 
     scores = tournament.play(
         continuation_probability=0.99654, repetitions=50, jobs=12
--- tournament/agents/axelrod_first.py
+++ tournament/agents/axelrod_first.py
@@ -9,7 +9,7 @@ from numpy.random import RandomState
 
 
 class Davis(Agent):
-    '''
+    """
     Submitted to Axelrod's first tournament by Morton Davis.
 
     The description written in [Axelrod1980]_ is:
@@ -22,19 +22,19 @@ class Davis(Agent):
     Names:
 
     - Davis: [Axelrod1980]_
-    '''
+    """
 
     def __init__(self, rounds_to_cooperate: int = 10):
-        '''
+        """
         rounds_to_cooperate: The number of rounds to play C.
-        '''
+        """
         super().__init__()
         self._rounds_to_cooperate = rounds_to_cooperate
 
     def play_move(self, history: List[Action], opp_history: List[Action]):
-        '''
+        """
         Begins by playing C, then plays D for the remaining rounds if the opponent ever plays D.
-        '''
+        """
         if len(history) < self._rounds_to_cooperate:
             #print("Davis: playing C")
             return Action.COOPERATE
@@ -153,3 +153,142 @@ class Graaskamp(Agent):
         
        #print("Graaskamp: playing C")
         return Action.COOPERATE
+
+
+class Shubik(Agent):
+    """
+   Submitted to Axelrod's first tournament by Martin Shubik.
+   The description written in [Axelrod1980]_ is:
+   > "This rule cooperates until the other defects, and then defects once. If
+   > the other defects again after the rule's cooperation is resumed, the rule
+   > defects twice. In general, the length of retaliation is increased by one for
+   > each departure from mutual cooperation. This rule is described with its
+   > strategic implications in Shubik (1970). Further treatment of its is given
+   > in Taylor (1976).
+   There is some room for interpretation as to how the strategy reacts to a
+   defection on the turn where it starts to cooperate once more. In Shubik
+   (1970) the strategy is described as:
+   > "I will play my move 1 to begin with and will continue to do so, so long
+   > as my information shows that the other player has chosen his move 1. If my
+   > information tells me he has used move 2, then I will use move 2 for the
+   > immediate k subsequent periods, after which I will resume using move 1. If
+   > he uses his move 2 again after I have resumed using move 1, then I will
+   > switch to move 2 for the k + 1 immediately subsequent periods . . . and so
+   > on, increasing my retaliation by an extra period for each departure from the
+   > (1, 1) steady state."
+   This is interpreted as:
+   The player cooperates, if when it is cooperating, the opponent defects it
+   defects for k rounds. After k rounds it starts cooperating again and
+   increments the value of k if the opponent defects again.
+   This strategy came 5th in Axelrod's original tournament.
+   Names:
+   - Shubik: [Axelrod1980]_
+   """
+
+    def __init__(self):
+        super().__init__()
+        self.is_retaliating = False
+        self.retaliation_length = 0
+        self.retaliation_remaining = 0
+
+    def _decrease_retaliation_counter(self):
+        """
+        Lower the remaining owed retaliation count and flip to non-retaliate
+        if the count drops to zero.
+        """
+        if self.is_retaliating:
+            self.retaliation_remaining -= 1
+            if self.retaliation_remaining == 0:
+                self.is_retaliating = False
+
+    def play_move(self, history: List[Action], opp_history: List[Action]) -> Action:
+        if not opp_history:
+            return Action.COOPERATE
+
+        if self.is_retaliating:
+            self._decrease_retaliation_counter()
+            return Action.DEFECT
+
+        if opp_history[-1] == Action.DEFECT and history[-1] == Action.COOPERATE:
+            self.is_retaliating = True
+            self.retaliation_length += 1
+            self.retaliation_remaining = self.retaliation_length
+            self._decrease_retaliation_counter()
+            return Action.DEFECT
+
+        return Action.COOPERATE
+
+class SteinAndRapoport(Agent):
+    """
+    Submitted to Axelrod's first tournament by William Stein and Amnon Rapoport.
+    The description written in [Axelrod1980]_ is:
+    > "This rule plays tit for tat except that it cooperates on the first four
+    > moves, it defects on the last two moves, and every fifteen moves it checks
+    > to see if the opponent seems to be playing randomly. This check uses a
+    > chi-squared test of the other's transition probabilities and also checks for
+    > alternating moves of CD and DC.
+    This is implemented as follows:
+    1. It cooperates for the first 4 moves.
+    2. It defects on the last 2 moves.
+    3. Every 15 moves it makes use of a `chi-squared
+       test <http://en.wikipedia.org/wiki/Chi-squared_test>`_ to check if the
+       opponent is playing randomly. If so it defects.
+    This strategy came 6th in Axelrod's original tournament.
+    Names:
+    - SteinAndRapoport: [Axelrod1980]_
+    """
+
+    def __init__(self, alpha: float = 0.05):
+        """
+        alpha: float
+            The significant level of p-value from chi-squared test with
+            alpha == 0.05 by default.
+        """
+        super().__init__()
+        self.alpha = alpha
+        self.opponent_is_random = False
+
+    def play_move(self, history: List[Action], opp_history: List[Action]) -> Action:
+        round_number = len(history) + 1
+
+        # First 4 moves
+        if round_number < 5:
+            return Action.COOPERATE
+
+        # For first 15 rounds tit for tat as we do not know opponents strategy
+        elif round_number < 15:
+            return opp_history[-1]
+
+        if round_number % 15 == 0:
+            cnt = Counter(opp_history)
+            num_c = cnt[Action.COOPERATE]
+            num_d = cnt[Action.DEFECT]
+            p_value = chisquare([num_c, num_d]).pvalue
+            self.opponent_is_random = p_value >= self.alpha
+
+        if self.opponent_is_random:
+            # Defect if opponent plays randomly
+            return Action.DEFECT
+        else:  # TitForTat if opponent plays not randomly
+            return opp_history[-1]
+
+        return Action.COOPERATE
+
+class Grudger(Agent):
+    def __init__(self):
+        super().__init__()
+        self.is_retaliation = False
+
+    def play_move(self, history: List[Action], opp_history: List[Action]) -> Action:
+        if not opp_history:
+            return Action.COOPERATE
+
+        if self.is_retaliation is True:
+            return Action.DEFECT
+
+        if opp_history[-1] == Action.DEFECT:
+            self.is_retaliation = True
+            return Action.DEFECT
+
+        return Action.COOPERATE
+
