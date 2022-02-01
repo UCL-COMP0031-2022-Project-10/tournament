fb23af0a77f1c5f1b3cc3aa22366968d5a6f1d56
Jeremy <jeremyloyingping@googlemail.com>
2022-02-01 01:29:46 +0000

    Refactor Pavlov slightly.

--- tournament/agents/pavlov.py
+++ tournament/agents/pavlov.py
@@ -5,17 +5,17 @@ from tournament.agent import Agent
 
 
 def rtsp_list(history: List[Action], opp_history: List[Action]) -> List[int]:
-    return_list = []
-    for i in range(len(history)):
-        if history[i] == Action.COOPERATE and opp_history[i] == Action.COOPERATE:
-            return_list.append(0)
-        elif history[i] == Action.DEFECT and opp_history[i] == Action.COOPERATE:
-            return_list.append(1)
-        elif history[i] == Action.COOPERATE and opp_history[i] == Action.DEFECT:
-            return_list.append(2)
-        else:  # if(history[i] == Action.COOPERATE and opp_history[i] == Action.COOPERATE)
-            return_list.append(3)
-    return return_list
+    result = []
+    for actions in zip(history, opp_history):
+        if actions == (Action.COOPERATE, Action.COOPERATE):
+            result.append(0)
+        elif actions == (Action.DEFECT, Action.COOPERATE):
+            result.append(1)
+        elif actions == (Action.COOPERATE, Action.DEFECT):
+            result.append(2)
+        else:
+            result.append(3)
+    return result
 
 
 class Pavlov(Agent):
@@ -24,10 +24,10 @@ class Pavlov(Agent):
         if not opp_history:
             return Action.COOPERATE
         else:
-            # previously same choice, then COOPERATE
+            # previously the same choice, then COOPERATE
             previous = rtsp_list(history, opp_history)[-1]
             if previous == 0 or previous == 3:
                 return Action.COOPERATE
             # previously different choice, then DEFECT
-            else:  # if rtsp_list[-1] == 1 or 2
+            else:
                 return Action.DEFECT
