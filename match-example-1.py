bbf2170781ebf4264ac62c972680969f2950dfda
Jeremy <jeremyloyingping@googlemail.com>
2022-02-04 16:19:14 +0000

    Update match-example-1.py

--- match-example-1.py
+++ match-example-1.py
@@ -8,4 +8,6 @@ for i, ((move1, move2), (score1, score2), (reward1, reward2)) in enumerate(
         continuation_probability=0.99654, limit=1000, noise=0
     )
 ):
-    print(f"{i:<4} : \t {move1:<20} {score1:<20} {move2:<20} {score2:<20}")
+    print(
+        f"{i:<4} | \t {move1:<20} {score1:<8} {f'({reward1})':<20} {move2:<20} {score2:<8} {f'({reward2})':<10}"
+    )
