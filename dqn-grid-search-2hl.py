b0d501e819adfade14f7ff485b09b361237779eb
Jeremy <jeremyloyingping@googlemail.com>
2022-03-09 02:52:30 +0000

    Create a results gathering script for DQN agents with two hidden layers.

--- dqn-grid-search-2hl.py
+++ dqn-grid-search-2hl.py
@@ -11,17 +11,19 @@ from tournament.gridsearch import evaluate
 
 
 class QNetwork(nn.Module):
-    def __init__(self, lookback, n1=32):
+    def __init__(self, lookback, n1=32, n2=32):
         super().__init__()
 
         self.flatten = nn.Flatten()
         self.layer1 = nn.Linear(2 * lookback, n1)
-        self.layer2 = nn.Linear(n1, n1)
-        self.layer3 = nn.Linear(n1, 2)
+        self.layer2 = nn.Linear(n1, n2)
+        self.layer3 = nn.Linear(n2, n1)
+        self.layer4 = nn.Linear(n1, 2)
 
         nn.init.kaiming_uniform_(self.layer1.weight, mode="fan_in", nonlinearity="relu")
         nn.init.kaiming_uniform_(self.layer2.weight, mode="fan_in", nonlinearity="relu")
         nn.init.kaiming_uniform_(self.layer3.weight, mode="fan_in", nonlinearity="relu")
+        nn.init.kaiming_uniform_(self.layer4.weight, mode="fan_in", nonlinearity="relu")
 
     def forward(self, x):
         x = x.unsqueeze(dim=0)
@@ -29,13 +31,14 @@ class QNetwork(nn.Module):
         x = torch.relu(self.layer1(x))
         x = torch.relu(self.layer2(x))
         x = torch.relu(self.layer3(x))
+        x = torch.relu(self.layer4(x))
 
         return x
 
 
 class DQN(DeepQLearner):
     def __init__(
-        self, lookback, n1, epsilon, epsilon_decay, learning_rate, discount_rate
+        self, lookback, n1, n2, epsilon, epsilon_decay, learning_rate, discount_rate
     ):
         super().__init__()
 
@@ -44,7 +47,7 @@ class DQN(DeepQLearner):
         self._epsilon_decay = epsilon_decay
         self._learning_rate = learning_rate
         self._discount_rate = discount_rate
-        self._q_network = QNetwork(self.lookback, n1)
+        self._q_network = QNetwork(self.lookback, n1, n2)
 
 
 def main():
@@ -54,12 +57,13 @@ def main():
     ]
 
     grid = {
-        "lookback": [1, 2, 4, 6, 8, 10],
-        "n1": [4, 8, 12, 16, 24, 32, 64, 96, 128],
-        "epsilon": [0.05, 0.1, 0.2],
+        "lookback": [4, 8],
+        "n1": [4, 8, 12, 16, 24, 32],
+        "n2": [4, 8, 12, 16, 24, 32],
+        "epsilon": [0.2],
         "epsilon_decay": [0.0],
         "learning_rate": [0.01, 0.001],
-        "discount_rate": [0.95, 0.99],
+        "discount_rate": [0.95],
     }
 
     results = []
@@ -79,7 +83,7 @@ def main():
     if results:
         df = pd.DataFrame(results)
         df["agents"] = ",".join([a.__name__ for a in agents])
-        df.to_csv(f"results/dqn-1hl-{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.csv")
+        df.to_csv(f"results/dqn-2hl-{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.csv")
 
 
 if __name__ == "__main__":
