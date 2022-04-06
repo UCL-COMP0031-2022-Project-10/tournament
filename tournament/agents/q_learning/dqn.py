4a4505db6eb23a3d46c51b818d726e76c38fe4a1
Jeremy <jeremyloyingping@googlemail.com>
2022-04-06 20:33:02 +0100

    Fix DQN state update bug.

--- tournament/agents/q_learning/dqn.py
+++ tournament/agents/q_learning/dqn.py
@@ -9,28 +9,21 @@ from tournament.agent import TrainableAgent
 
 torch.manual_seed(42)
 
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class QNetwork(nn.Module):
     def __init__(self, lookback):
         super().__init__()
 
         self.flatten = nn.Flatten()
-        # self.layer1 = nn.Linear(2 * lookback, lookback)
-        # self.layer2 = nn.Linear(lookback, lookback)
-        # self.layer3 = nn.Linear(lookback, 2)
-
-        # self.layer1 = nn.Linear(2 * lookback, 32)
-        # self.layer2 = nn.Linear(32, 64)
-        # self.layer3 = nn.Linear(64, 32)
-        # self.layer4 = nn.Linear(32, 2)
-
         self.layer1 = nn.Linear(2 * lookback, 32)
         self.layer2 = nn.Linear(32, 32)
         self.layer3 = nn.Linear(32, 2)
 
-        # self.layer1 = nn.Linear(2 * lookback, 16)
-        # self.layer2 = nn.Linear(16, 16)
-        # self.layer3 = nn.Linear(16, 2)
+        nn.init.kaiming_uniform_(self.layer1.weight, mode="fan_in", nonlinearity="relu")
+        nn.init.kaiming_uniform_(self.layer2.weight, mode="fan_in", nonlinearity="relu")
+        nn.init.kaiming_uniform_(self.layer3.weight, mode="fan_in", nonlinearity="relu")
 
     def forward(self, x):
         x = x.unsqueeze(dim=0)
@@ -38,22 +31,22 @@ class QNetwork(nn.Module):
         x = torch.relu(self.layer1(x))
         x = torch.relu(self.layer2(x))
         x = torch.relu(self.layer3(x))
-        # x = torch.relu(self.layer4(x))
 
         return x
 
 
 class DeepQLearner(TrainableAgent):
-    lookback = 1
-    epsilon = 0.2
-    decay_limit = 0.05
-
     def __init__(self) -> None:
+
+        self.lookback = 1
+        self.epsilon = 0.2
+
         self._discount_rate = 0.95
         self._learning_rate = 0.008
         self._epsilon_decay = 0.0  #  0.002
-        self._evaluation_epsilon = 0.001
+        self._evaluation_epsilon = 0.0
         self._epsilon = self._evaluation_epsilon
+        self._decay_limit = 0.05
 
         self._loss = 0
         self._games = 0  # TODO: remove
@@ -64,33 +57,35 @@ class DeepQLearner(TrainableAgent):
         self._state = None
         self._q_network = QNetwork(self.lookback)
 
-    def on_match_start(self):
+    def get_intial_state(self):
         # randomly initialise the state
-        self._state = torch.randint(
-            low=0, high=2, size=(self.lookback, 2), dtype=torch.float32
-        )
+        # self._state = torch.randint(
+        #     low=0, high=2, size=(self.lookback, 2), dtype=torch.float32
+        # )
+
+        # return a nice state
+        return torch.zeros(size=(self.lookback, 2), dtype=torch.float32)
+
+    def on_match_start(self):
+        self._state = self.get_intial_state()
 
     def notify_prematch(self):
         self._epsilon = self.epsilon
-        self._decay_limit = self.decay_limit
         self._optimiser.zero_grad()
 
     def notify_postmatch(self):
         self._games += 1
-        # print(
-        #     f"POSTMATCH {self._games}:",
-        #     self._values,
-        # )
+        self._epsilon = self._evaluation_epsilon
 
     @property
     def metric(self):
         return self._loss / self._count if self._count > 0 else None
 
     def setup(self) -> None:
-        try:
-            self._q_network.load_state_dict(torch.load("model.pt"))
-        except:
-            pass
+        # try:
+        #     self._q_network.load_state_dict(torch.load("model.pt"))
+        # except:
+        #     pass
 
         self._criterion = torch.nn.HuberLoss()
         self._optimiser = optim.Adam(
@@ -114,12 +109,21 @@ class DeepQLearner(TrainableAgent):
             Action: The action to be performed.
         """
 
+        self._prev_state = self._state.clone()
+        if history:
+            self._state = torch.cat(
+                (
+                    self._prev_state[1:],
+                    torch.tensor([[history[-1].value, opp_history[-1].value]]),
+                )
+            )
+
         # slowly reduce the exploration rate when above the decay limit
         if self._epsilon > self._decay_limit:
             self._epsilon -= self._epsilon_decay
 
         # get the Q-values from the model
-        self._values = self._q_network(self._state)
+        self._values = self._q_network(self._prev_state)
 
         # exploratory moves are picked uniformly at random with probability self._epsilon
         if random() < self._epsilon:
@@ -150,11 +154,7 @@ class DeepQLearner(TrainableAgent):
             rewards (Tuple[float, float]): The rewards from the actions just performed
         """
 
-        move = moves[0].value
-        state = self._state.clone()
-        self._state = torch.cat((state[1:], torch.tensor([[move, moves[1].value]])))
-
-        prediction = self._values[0, move]
+        prediction = self._values[0, moves[0].value]
         target = (
             rewards[0] + self._discount_rate * self._q_network(self._state)[0].max()
         )
@@ -170,46 +170,3 @@ class DeepQLearner(TrainableAgent):
             self._optimiser.zero_grad()
 
         self._count += 1
-
-
-class SingleLookback(DeepQLearner):
-    pass
-
-
-class DoubleLookback(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self.lookback = 2
-        self._epsilon = 0.25
-
-
-class TripleLookback(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self.lookback = 3
-        self._epsilon = 0.2
-
-
-class LargeLookback(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self.lookback = 16
-        self.epsilon = 0.1
-
-
-class HighExplorationRate(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self.epsilon = 0.25
-
-
-class LowExplorationRate(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self.epsilon = self._decay_limit
-
-
-class LowDiscountRate(DeepQLearner):
-    def __init__(self):
-        super().__init__()
-        self._discount_rate = 0.75
