fd396890a1138d66a3964435aa5ad53275ac2433
zhuzihan728 <zcabhub@ucl.ac.uk>
2022-04-07 15:20:17 +0100

    merge tabular against 1st tnment agents

--- merge_csv.py
+++ merge_csv.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 import os
-Folder_Path = r'results/dqn'          
+Folder_Path = r'results/tabular'          
 SaveFile_Name = r'all.csv'             
-file2 = r'dqn-1hl-2022-04-07 01-43-25.csv'
-file1 = r'dqn-1hl-2022-04-07 01-03-04.csv'
+file2 = r'tabular-2022-04-06 20-21-41.csv'
+file1 = r'tabular-2022-04-07 02-01-19.csv'
  
 os.chdir(Folder_Path)
 
@@ -12,6 +12,5 @@ df = pd.read_csv(file1)
  
 df.to_csv(SaveFile_Name,encoding="utf_8_sig",index=False)
  
-#循环遍历列表中各个CSV文件名，并追加到合并后的文件
 df = pd.read_csv(file2)
 df.to_csv(SaveFile_Name,encoding="utf_8_sig",index=False, header=False, mode='a+')
