3e1059173b64c7f6e29c9282350c688ff5d5ed95
Anirudh Lakra <55798132+anirudh1666@users.noreply.github.com>
2022-04-07 11:10:47 +0100

    Started analysis of new Tabular dataset

--- rnn-grid-search-1hl.py
+++ rnn-grid-search-1hl.py
@@ -8,16 +8,30 @@ import torch.nn as nn
 from tournament.agents.q_learning.rnn import RNNQLearner
 from tournament.agents.tft import OmegaTFT, TitForTat
 from tournament.agents.axelrod_first import (
+    Davis,
     Downing,
+    Feld,
+    Grofman,
+    Grudger,
+    Joss,
     Nydegger,
-    TidemanAndChieruzzi
-    )
+    Shubik,
+    SteinAndRapoport,
+    TidemanAndChieruzzi,
+    Tullock,
+)
 from tournament.agents.axelrod_second import (
-    Champion,
     Borufsen,
-    SecondByGraaskampKatzen
-    )
-from tournament.gridsearch import evaluate
+    Champion,
+    Leyvraz,
+    SecondByBlackK83R,
+    SecondByGraaskampKatzen,
+    SecondByHarrington,
+    SecondByTidemanAndChieruzzi,
+    SecondByWeiner,
+    SecondByWhiteK72R,
+)
+from tournament.gridsearch import train_and_evaluate
 
 class RNN(nn.Module):
     def __init__(
@@ -64,47 +78,71 @@ class RNNLearner(RNNQLearner):
         self._q_network = RNN(self.lookback, hidden_size, num_layers)
 
 def main():
-    agents = [
+    agents = agents = [
         TitForTat,
-        Nydegger,
-        Downing,
         TidemanAndChieruzzi,
-        Champion,
         Borufsen,
-        SecondByGraaskampKatzen
-        ]
-    grid = {
-        "lookback": [1, 2, 4, 6, 8],
-        "hidden_size": [4, 32, 64],
-        "num_layers": [1],
-        "epsilon": [0.1],
-        "epsilon_decay": [0.0],
-        "learning_rate": [0.01],
-        "discount_rate": [0.99]
-        }
+        SecondByGraaskampKatzen,
+        Nydegger,
+        Grofman,
+        Shubik,
+        Champion,
+        Leyvraz
+        ] 
 
+    grid = {
+        "lookback": [1, 2, 4, 8],
+        "hidden_size": [8,12,16,24],
+        "epsilon": [0.05, 0.1, 0.2],
+        "num_layers" : [1],
+        "epsilon_decay": [0.0],
+        "learning_rate": [0.001, 0.01, 0.05, 0.1],
+        "discount_rate": [0.95, 0.99],
+    }
+
+    d = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
     results = []
     try:
-        # unpack all values in grid. take cartesian product (all possible combs)
-        # and evaluate.
-        for hyperparameters in itertools.product(*grid.values()):
+        space = list(itertools.product(*grid.values()))
+        size = len(space)
+        for i, hyperparameters in enumerate(space):
             print(
-                f"[{datetime.now().strftime('%y-%m-%d %H-%M-%S')}]",
+                f"[{datetime.now().strftime('%Y-%m-%d %H-%M-%S')} | {i + 1}/{size}]",
                 *hyperparameters,
-                sep="\t"
+                sep="\t",
+            )
+            result, agent = train_and_evaluate(
+                agents, RNNLearner, epochs=250, **dict(zip(grid.keys(), hyperparameters))
+            )
+            results.append(result)
+            print(
+                f"[{datetime.now().strftime('%Y-%m-%d %H-%M-%S')} | {i + 1}/{size}]",
+                f"COOP%={results[-1]['tr_cooperation_percentage']}",
+                f"LOSS={results[-1]['tr_final_loss']}",
+                f"RANK={results[-1]['tn_rank']}",
+                f"SCORE={results[-1]['tn_mean_score']}",
+                sep="\t",
+            )
+            if result["tn_mean_score"] > 750 or result["tn_rank"] > 27:
+                torch.save(
+                    agent._q_network.state_dict(),
+                    f"models/dqn/{d} - 1hl - {i} - {result['tn_mean_score']} - {result['tn_rank']}.pt",
                 )
-            results.append(
-                evaluate(agents, RNNLearner, **dict(zip(grid.keys(), hyperparameters)))
-                )
+                with open(
+                    f"models/dqn/{d} - 1hl - {i} - {result['tn_mean_score']} - {result['tn_rank']}.txt",
+                    "w",
+                ) as f:
+                    f.write(dumps(result))
+    except Exception as e:
+        print("Qutting evaluation early:", str(e))
     except:
-        print("Quitting evaluation early.")
+        print("Quitting evaluation early")
 
     if results:
         df = pd.DataFrame(results)
         df["agents"] = ",".join([a.__name__ for a in agents])
-        df.to_csv(f"results/rnn-{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.csv")
-            
-    
+        df.to_csv(f"results/dqn/dqn-1hl-{d}.csv")
+
+
 if __name__ == "__main__":
-    torch.set_num_threads(12)
     main()
